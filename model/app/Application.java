d2269bc24407c63fca7cfa8720a88272365bb202
Elise tarres <elise.tarres@studentfr.ch>
2025-01-31 15:19:41 +0100

    devoir 01

---
+++ model/app/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+    public static void main(String[] args) {
+        Amis ami1 = new Amis("Jul", 5);
+        Amis ami2 = new Amis("Dylan", 2);
+        Amis ami3 = new Amis("Tomas", 6);
+
+        ami1.boitUneBierre();
+        ami2.boitUneBierre();
+        ami3.boitUneBierre();
+
+        
+    }
+}
